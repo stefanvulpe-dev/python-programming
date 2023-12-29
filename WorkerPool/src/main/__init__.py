f95f2325d318c839f8d48e0d5bdb0d8024d5b687
Stefan Vulpe <svulpe25@gmail.com>
2023-12-29 12:57:29 +0200

    refactor(docs): modify documentation sections

--- WorkerPool/src/main/__init__.py
+++ WorkerPool/src/main/__init__.py
@@ -4,15 +4,15 @@ This module contains the main scripts of the project.
 
 Functions:
 
-parse_args: parses the command line arguments
-read_input_data: reads the input data from a json file
-populate_queue: populates the queue with the input data
-spawn_workers: spawns the workers
-save_webpage: saves a webpage to disk
-download_webpage: downloads a webpage
-process_job: processes a job
-work: main function for the worker process
-main: main function
+- parse_args: parses the command line arguments
+- read_input_data: reads the input data from a json file
+- populate_queue: populates the queue with the input data
+- spawn_workers: spawns the workers
+- save_webpage: saves a webpage to disk
+- download_webpage: downloads a webpage
+- process_job: processes a job
+- work: main function for the worker process
+- main: main function
 
 Author:
 Stefan Vulpe
